60d57aa82863aabc5ab2ddb67f3cd934558cfb94
dan2344 <america_emerson7@hotmail.com>
2016-09-22 08:49:22 -0500

    Add files via upload

---
+++ Examen/src/Examen/Main.java
@@ -0,0 +1,22 @@
+
+package Examen;
+
+/**
+ * Ejercicio: Calculadora basica 
+ * Implementar la Interfaz de usuario 
+ * Implementar los metodos siguientes a través de los listeners adecuados: 
+ * - Sumar 
+ * - Restar 
+ * - Multiplicar 
+ * - Dividir
+ * 
+ *@author:  emmanuel 
+ * @version:  1.0 
+ * @date: 06-09-2015 
+ */
+public class Main {
+ public static void main(String[] args) {
+		Examen calculadora = new Examen();
+		calculadora.setVisible(true);
+	}   
+}
